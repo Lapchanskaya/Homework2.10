8e2dbfebd44f1c378ece9129e3bf5f6eee68f86f
Svetlana <isaeva322@mail.ru>
2023-02-19 19:07:13 +0300

    Задание № 3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
@@ -51,6 +52,33 @@ public class Main {
         Consumer<String> welcomeName1 = name -> System.out.println(" Добро пожаловать," + name + "!" );
         welcomeName1.accept("Пушкин Александр Сергеевич");
         welcomeName1.accept ( "Матвейка007");
+
+
+        System.out.println("   *** Задача № 3 ***   ");
+        System.out.println(" Реализация через анонимный класс ");
+
+        Function<Double, Long> number = new Function<>() {
+            @Override
+            public Long apply(Double quantity) {
+                return Math.round(quantity);
+            }
+        };
+        System.out.println( number.apply(3.141592653));
+        System.out.println( number.apply(5.8));
+        System.out.println( number.apply(0.897));
+
+
+        printSeparation();
+
+        System.out.println(" Реализация через лямбду-выражение ");
+
+        Function<Double, Long> number1 = quantity -> Math.round(quantity);
+        System.out.println( number1.apply(1051.525));
+        System.out.println( number1.apply(8957.012547));
+
+
+
+
 
 
 
