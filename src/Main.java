adfe4e4ae7c7c53d79d71bbdec8f4112ce7962ac
Svetlana <isaeva322@mail.ru>
2023-02-19 18:57:02 +0300

    Задание № 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("   *** Задача № 1 ***   ");
-        System.out.println("    Реализация через анонимный класс    ");
+        System.out.println(" Реализация через анонимный класс ");
 
         Predicate<Integer> plusNumbers = new Predicate<>() {
             @Override
@@ -22,11 +23,37 @@ public class Main {
 
         printSeparation();
 
-        System.out.println("    Реализация через лямбду-выражение    ");
+        System.out.println(" Реализация через лямбду-выражение ");
 
         Predicate<Integer> plusNumbers1 = integer -> (integer >= 0.0);
         System.out.println(plusNumbers1.test(50));
         System.out.println(plusNumbers1.test(-9));
+
+
+        System.out.println("   *** Задача № 2 ***   ");
+        System.out.println(" Реализация через анонимный класс ");
+
+        Consumer<String> welcomeName = new Consumer<>() {
+            @Override
+            public void accept(String name) {
+                System.out.println( " Добро пожаловать," + name + "!");
+
+            }
+        };
+        welcomeName.accept("Нина Владимировна");
+        welcomeName.accept("Аркадий");
+
+
+        printSeparation();
+
+        System.out.println(" Реализация через лямбду-выражение ");
+
+        Consumer<String> welcomeName1 = name -> System.out.println(" Добро пожаловать," + name + "!" );
+        welcomeName1.accept("Пушкин Александр Сергеевич");
+        welcomeName1.accept ( "Матвейка007");
+
+
+
 
 
 
