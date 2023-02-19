48eea4c0e2e70d28c788998e8322bf5fd85632de
Svetlana <isaeva322@mail.ru>
2023-02-19 18:48:03 +0300

    Задание № 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.function.Predicate;
+
 public class Main {
+
+    public static void printSeparation() {
+        System.out.println(" ================== ");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("   *** Задача № 1 ***   ");
+        System.out.println("    Реализация через анонимный класс    ");
+
+        Predicate<Integer> plusNumbers = new Predicate<>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer >= 0;
+            }
+        };
+        System.out.println(plusNumbers.test(1));
+        System.out.println(plusNumbers.test(-25));
+
+        printSeparation();
+
+        System.out.println("    Реализация через лямбду-выражение    ");
+
+        Predicate<Integer> plusNumbers1 = integer -> (integer >= 0.0);
+        System.out.println(plusNumbers1.test(50));
+        System.out.println(plusNumbers1.test(-9));
+
+
+
+
+
     }
 }
